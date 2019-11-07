a652afda4cae141ed4809ad00636bddf502c45eb
Michael Wilson <mwilson@fivestardev.com>
2019-11-07 09:30:23 -0500

    update email copy

--- app/mailers/mentorship_mailer.rb
+++ app/mailers/mentorship_mailer.rb
@@ -4,13 +4,13 @@ class MentorshipMailer < ActionMailer::Base
     def acceptance_email_to_mentee
 		@mentor = User.find(params[:mentor][:user_id])
 		@mentee = User.find(params[:mentee][:user_id])
-        mail(to: @mentee.email, subject: 'You have a new mentor!')
+        mail(to: @mentee.email, subject: 'A mentor has requested contact in the RedChairPGH Mentor Site!')
     end
 
     def acceptance_email_to_mentor
 		@mentor = User.find(params[:mentor][:user_id])
 		@mentee = User.find(params[:mentee][:user_id])
-        mail(to: @mentor.email, subject: 'You have a new mentee!')
+        mail(to: @mentor.email, subject: 'You have a new mentee request in the RedChairPGH Mentor Site!')
     end
 
 	def tester
